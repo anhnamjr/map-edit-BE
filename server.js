4b3c34a607078f9a6e065f2eaf2a4469d7e5eaaf
Nguyen Phuc Manh <phucmanh1999@gmail.com>
2021-06-16 23:19:15 +0700

    fix cors err

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const authRouter = require("./routes/auth.router");
 const verifyToken = require("./auth/verify.token");
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
+
 const PORT = 3001;
 
 app.use(
